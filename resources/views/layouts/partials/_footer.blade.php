aae86ebfaa17830a740b5ac403cc901cc9186743
revosftw <sahay.rohan@gmail.com>
2018-07-02 02:34:56 +0530

    Initial commit

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+  <span style="left: 45%;position: relative;">Made with <span style="color: #e74c3c">&hearts;</span> by <a href="https://github.com/revosftw" id="revosftw">revosftw</a>.</span>
+</footer>
---
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-light bg-light">
+	<a class="navbar-brand">theQuizzer</a>
+	<form class="form-inline">
+		<div class="input-group">
+			<div class="input-group-prepend">
+				<span class="input-group-text" id="basic-addon-1">@</span>
+			</div>
+			<input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" name="username">
+		</div>
+	</form>
+</nav>
