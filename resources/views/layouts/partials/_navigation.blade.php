c591811d1a88c7283c7160a86c77bd8e63474e51
revosftw <sahay.rohan@gmail.com>
2018-07-03 09:33:38 +0530

    Initial commit

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -7,5 +7,11 @@
 			</div>
 			<input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" name="username">
 		</div>
+		<div class="input-group">
+			<div class="input-group-prepend">
+				<span class="input-group-text" id="basic-addon-1">@</span>
+			</div>
+			<input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" name="password">
+		</div>
 	</form>
 </nav>
