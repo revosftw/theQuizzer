4f28ae0febe3ece070dba4a7157158713f94b06f
revosftw <sahay.rohan@iamgod.in>
2018-09-10 00:52:54 +0530

    Fixed classname error

--- app/question.php
+++ app/question.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class questions extends Model
+class question extends Model
 {
    /**
     * Return the relationship between question and options.
