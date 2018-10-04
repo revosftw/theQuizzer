88b2d10a6c38122709938d86090a973ed2480828
revosftw <revosftw@maildrop.cc>
2018-10-04 10:22:42 +0530

    Initial Commit

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Report;
+use App\Exam;
 use Illuminate\Http\Request;
 
-class ReportController extends Controller
+class ExamController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -41,10 +41,10 @@ class ReportController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Report  $report
+     * @param  \App\Exam  $exam
      * @return \Illuminate\Http\Response
      */
-    public function show(Report $report)
+    public function show(Exam $exam)
     {
         //
     }
@@ -52,10 +52,10 @@ class ReportController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Report  $report
+     * @param  \App\Exam  $exam
      * @return \Illuminate\Http\Response
      */
-    public function edit(Report $report)
+    public function edit(Exam $exam)
     {
         //
     }
@@ -64,10 +64,10 @@ class ReportController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Report  $report
+     * @param  \App\Exam  $exam
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Report $report)
+    public function update(Request $request, Exam $exam)
     {
         //
     }
@@ -75,10 +75,10 @@ class ReportController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Report  $report
+     * @param  \App\Exam  $exam
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Report $report)
+    public function destroy(Exam $exam)
     {
         //
     }
