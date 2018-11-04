1642c69f45a40a28a220ac8150ab44d926a954d5
Rohan Sahay <sahay.rohan@iamgod.in>
2018-11-04 11:49:38 +0530

    fixed footer style

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,3 +1,5 @@
-<footer style="bottom: 0">
-  <span style="left: 45%;position: relative;">Made with <span style="color: #e74c3c">&hearts;</span> by <a href="https://github.com/revosftw" id="revosftw">revosftw</a>.</span>
+<footer class="fixed-bottom">
+  <div class="container text-center">
+    <span class="mx-auto">Made with <span style="color: #e74c3c">&hearts;</span> by <a href="https://github.com/revosftw" id="revosftw">revosftw</a>.</span>
+  </div>
 </footer>
