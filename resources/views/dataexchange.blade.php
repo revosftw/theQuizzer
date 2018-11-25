87e3e645874f6dbc79b77817244536bbfab30637
Rohan Sahay <sahay.rohan@iamgod.in>
2018-11-25 09:38:12 +0530

    Change id for files

--- resources/views/dataexchange.blade.php
+++ resources/views/dataexchange.blade.php
@@ -6,11 +6,35 @@
 
 <form>
   <div class="form-group row">
-    <div class="col-sm-8 mx-auto">
-      <label for="customFile" class="col-sm-3 col-md-3 col-form-label">Upload Categories</label>
+    <div class="col-sm-8 justify-content-center">
+      <label for="userFile" class="col-sm-3 col-md-3 col-form-label">Upload Users</label>
       <div class="custom-file col-sm-5 col-md-5 col-form-label" >
-        <input type="file" class="custom-file-input" id="customFile">
-        <label class="custom-file-label" for="customFile">Choose file</label>
+        <input type="file" class="custom-file-input" id="userFile">
+        <label class="custom-file-label" for="userFile">Choose file</label>
+      </div>
+    </div>
+  </div>
+</form>
+
+<form>
+  <div class="form-group row">
+    <div class="col-sm-8 justify-content-center">
+      <label for="questionFile" class="col-sm-3 col-md-3 col-form-label">Upload Questions</label>
+      <div class="custom-file col-sm-5 col-md-5 col-form-label" >
+        <input type="file" class="custom-file-input" id="questionFile">
+        <label class="custom-file-label" for="questionFile">Choose file</label>
+      </div>
+    </div>
+  </div>
+</form>
+
+<form>
+  <div class="form-group row">
+    <div class="col-sm-8 justify-content-center">
+      <label for="categoryFile" class="col-sm-3 col-md-3 col-form-label">Upload Categories</label>
+      <div class="custom-file col-sm-5 col-md-5 col-form-label" >
+        <input type="file" class="custom-file-input" id="categoryFile">
+        <label class="custom-file-label" for="categoryFile">Choose file</label>
       </div>
     </div>
   </div>
